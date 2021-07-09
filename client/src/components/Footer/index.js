eb85fcea55edf769160d79cee35b9971ccb8eb0d
Benjamin Harris <harben31@gmail.com>
2021-07-08 20:01:33 -0700

    added link for resume

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -9,7 +9,7 @@ const Footer = (props) => {
             <ul>
                 <li>LinkedIn: <a href='https://www.linkedin.com/in/benjamin-harris-81281a2b/'>benjamin-harris-81281a2b/</a></li>
                 <li>GitHub: <a href='https://github.com/harben31/'>harben31</a></li>
-                <li>Resume: <a href='#'>click here</a></li>
+                <li>Resume: <a href='https://drive.google.com/file/d/1h07cZ7M4e1cfOu6OUAMBvK7AO0GbBDWa/view?usp=sharing'>click here</a></li>
                 <li>Email: <a href='mailto:harben31@gmail.com'>harben31@gmail.com</a></li>
                 <li>Telephone: <a href='tel:512-815-0281'>(512) 815-0281</a></li>
             </ul>
